764cd418d6469f74763de49303f522184a4c951e
PC <robertopalomo03@gmai.com>
2025-01-16 00:47:45 +0100

    User table Updated

--- src/main/java/com/RobertoPalMol/TribeMe_Backend/Entity/User.java
+++ src/main/java/com/RobertoPalMol/TribeMe_Backend/Entity/User.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "\"user\"")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
